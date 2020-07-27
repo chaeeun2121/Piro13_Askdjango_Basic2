d3c75f6cac33a794cec925132d4c1fec6690ce16
ohchaeeun <oce7803@naver.com>
2020-07-27 19:33:45 +0900

    08

--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,8 @@ from .forms import SignupForm
 #             user = form.save()
 #             #로그인 처리
 #             auth_login(request, user)
-#             return redirect('profile')
+#             next_url = request.GET.get('next') or 'profile'
+#             return redirect(next_url)
 #     else:
 #         form = SignupForm()
 #     return render(request, 'accounts/signup.html',{
@@ -28,7 +29,8 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
     def form_valid(self, form):
         user = form.save()
